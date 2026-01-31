392a0e2e9580d12d3d477c2e22eefbaae2ed6324
LAPTOP-07HTCDKS\moham <mohamadagung007@gmail.com>
2026-01-31 14:58:31 +0700

    fix - trigger config

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,6 +2,7 @@
 // https://trigger.dev/docs
 
 import { defineConfig } from "@trigger.dev/sdk";
+import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 
 export default defineConfig({
   project: process.env.TRIGGER_PROJECT_ID || "proj_thamrdhirlwxzrzexewy",
@@ -19,4 +20,13 @@ export default defineConfig({
     },
   },
   dirs: ["./src/trigger"],
+  build: {
+    extensions: [
+      prismaExtension({
+        mode: "legacy",
+        schema: "./prisma/schema.prisma",
+        migrate: false, // Set to true if you want to run migrations during deployment
+      }),
+    ],
+  },
 });
